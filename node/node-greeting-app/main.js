0b665fb8fd15301052da2aa04e2faedd0d291a33
codedubs <35834450+codedubs@users.noreply.github.com>
2019-04-04 03:36:02 -0700

    Basic Node App: complete

---
+++ node/node-greeting-app/main.js
@@ -0,0 +1,16 @@
+
+
+const greetings = require("./greeting.js");
+const sayings = require("./sayings.js");
+
+greetings.greetingEnglish();
+
+greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+
+greetings.greetingIcelandic();
+
+sayings.engSay();
+
+sayings.spanSay();
---
+++ node/node-greeting-app/sayings.js
@@ -0,0 +1,9 @@
+
+
+exports.engSay = function() {
+  console.log('How are you?');
+}
+
+exports.spanSay = function() {
+  console.log('Como estas?');
+}
